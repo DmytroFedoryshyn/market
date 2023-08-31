d3bb3582b564b680332f9c730f765a1bafa370e1
WallRun <dimonty2003@gmail.com>
2023-08-31 13:42:00 +0300

    updated a solution

--- src/main/java/com/example/OrderBook.java
+++ src/main/java/com/example/OrderBook.java
@@ -21,16 +21,18 @@ public class OrderBook {
         mapToUpdate.put(price, size);
     }
 
-    public String getBestBid() {
-        long price = bids.firstKey();
-        long size = bids.get(price);
+    public String getBest(String type) {
+        TreeMap<Long, Long> map = type.equals("best_bid") ? bids : asks;
 
-        return price + "," + size;
-    }
-
-    public String getBestAsk() {
-        long price = asks.firstKey();
-        long size = asks.get(price);
+        long price = 0;
+        long size = 0;
+        for (Map.Entry<Long, Long> entry : map.entrySet()) {
+            price = entry.getKey();
+            size = entry.getValue();
+            if (entry.getValue() != 0) {
+                break;
+            }
+        }
 
         return price + "," + size;
     }
@@ -39,44 +41,45 @@ public class OrderBook {
         long bidSize = bids.getOrDefault(price, 0L);
         long askSize = asks.getOrDefault(price, 0L);
 
-        if (bidSize == 0 && askSize == 0) {
-            return "0";
+        if (bidSize == 0) {
+            return String.valueOf(askSize);
+        } else {
+            return String.valueOf(bidSize);
         }
-
-        return (bidSize + askSize) + "";
     }
 
     public void processMarketOrder(String[] order) {
-        long size = Long.parseLong(order[2]);
+        long orderSize = Long.parseLong(order[2]);
         String type = order[1];
 
-        if (type.equals("buy")) {
-            while (!asks.isEmpty() && size > 0) {
-                long lowestAsk = asks.firstKey();
-                long availableSize = asks.get(lowestAsk);
+        TreeMap<Long, Long> map = type.equals("buy") ? asks : bids;
 
-                if (availableSize <= size) {
-                    asks.remove(lowestAsk);
-                    size -= availableSize;
-                } else {
-                    asks.put(lowestAsk, availableSize - size);
-                    size = 0;
-                }
-            }
-        } else if (type.equals("sell")) {
-            while (!bids.isEmpty() && size > 0) {
-                long highestBid = bids.firstKey();
-                long availableSize = bids.get(highestBid);
+        while (orderSize > 0) {
+            long best = getBestPrice(type);
+            long availableSize = map.get(best);
 
-                if (availableSize <= size) {
-                    bids.remove(highestBid);
-                    size -= availableSize;
-                } else {
-                    bids.put(highestBid, availableSize - size);
-                    size = 0;
-                }
+            if (availableSize <= orderSize) {
+                map.remove(best);
+                orderSize -= availableSize;
+            } else {
+                map.put(best, availableSize - orderSize);
+                orderSize = 0;
             }
         }
+    }
+
+    public long getBestPrice(String type) {
+        TreeMap<Long, Long> map = type.equals("buy") ? asks : bids;
+
+        long price = 0;
+        for (Map.Entry<Long, Long> entry : map.entrySet()) {
+            price = entry.getKey();
+            if (entry.getValue() != 0) {
+                break;
+            }
+        }
+
+        return price;
     }
 }
 
@@ -88,7 +91,7 @@ class MarketSimulator {
         String line;
 
         while ((line = br.readLine()) != null) {
-            String[] order = line.split(",");
+            String[] order = splitLine(line);
             String type = order[0];
 
             if (type.equals("u")) {
@@ -97,10 +100,8 @@ class MarketSimulator {
                 String queryType = order[1];
                 String result = "";
 
-                if (queryType.equals("best_bid")) {
-                    result = orderBook.getBestBid();
-                } else if (queryType.equals("best_ask")) {
-                    result = orderBook.getBestAsk();
+                if (queryType.equals("best_bid") || queryType.equals("best_ask")) {
+                    result = orderBook.getBest(queryType);
                 } else if (queryType.equals("size")) {
                     long price = Long.parseLong(order[2]);
                     result = orderBook.getSizeAtPrice(price);
@@ -114,5 +115,19 @@ class MarketSimulator {
         }
 
         wr.close();
+        br.close();
+    }
+
+    private static String[] splitLine(String line) {
+        int start = 0;
+        int end;
+        int index = 0;
+        String[] result = new String[4];
+        while ((end = line.indexOf(',', start)) != -1) {
+            result[index++] = line.substring(start, end);
+            start = end + 1;
+        }
+        result[index] = line.substring(start);
+        return result;
     }
 }
